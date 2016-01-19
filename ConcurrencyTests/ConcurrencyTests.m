1528689c5a76d1c778554adff1c4c36c2bbb4f1b
Marcin Matczuk <mmatczuk@cogniance.com>
2016-01-19 17:32:02 +0100

    testing jenkins multijobs

---
+++ ConcurrencyTests/ConcurrencyTests.m
@@ -0,0 +1,40 @@
+//
+//  ConcurrencyTests.m
+//  ConcurrencyTests
+//
+//  Created by Marcin Matczuk on 19/01/16.
+//  Copyright © 2016 Charcoal Design. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ConcurrencyTests : XCTestCase
+
+@end
+
+@implementation ConcurrencyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(YES, @"no way!");
+}
+
+- (void)_testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
